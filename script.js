b88d1e124a2522f086d1370cc99356dc104366b9
Rana-TANRIKULU <ranatanrikuluu@gmail.com>
2022-06-13 17:27:27 +0300

    ikinci videolarım

---
+++ script.js
@@ -0,0 +1,100 @@
+// var age;
+// console.log(age);
+// age=11
+// console.log(age);
+
+// var fullname='ceyda tanrıkulu';
+// console.log(fullname);
+
+// fullname='Yavuz cangür';
+// console.log(fullname);
+
+
+// //Değişken Tanımlama Kuralları
+
+// //Sayısal İfade ile başlayamaz
+// //windows-nokta
+
+
+// //Komut isimleriyle tanımlama yapılamaz.
+// //Birden fazla kelime
+
+// var ad='Rana';
+// var soyad='Tanrıkulu';
+
+// var ad_soyad='Rana Tanrıkulu';
+// var adSoyad="Cayda Tanrıkulu";
+
+// //case sensetive (BÜYÜK küçük duyarlılığı)
+
+// var firstname='Rana';
+// var Firstname='Cedo';
+
+// console.log(firstname);
+// console.log(Firstname);
+
+
+// //var,let,const
+
+// let city='ankara';
+// console.log(city);
+
+// const email='abc@gmail.com';
+// console.log(email);
+
+//Ders 3:Değişken Tipleri
+
+//Primitive Types
+
+//String
+let firstname='cedo';
+console.log(typeof firstname);
+
+//Number
+
+let age = 11;
+let money=100.5;
+console.log(typeof age);
+console.log(typeof money);
+
+
+//Boolean
+let isActive =false;
+
+
+console.log(typeof isActive)
+
+
+//null
+let job=null;
+
+console.log(typeof job)
+
+//undefined
+let car;
+console.log(typeof car)
+
+//Reference Types-Objects
+
+
+//Array
+let names=['ali','can','ahmet' ];
+
+console.log(typeof names);
+
+//object
+
+let address= {
+    city:'aydın',
+    country:'turkey'
+}
+
+console.log(typeof address);
+
+//Function
+
+var calculateAge= function(){
+    return 0;
+};
+
+console.log(typeof calculateAge);
