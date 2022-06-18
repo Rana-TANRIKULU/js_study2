282800aff9800f7ceb123a27f5abc3ba7138f1f1
Rana-TANRIKULU <ranatanrikuluu@gmail.com>
2022-06-18 15:08:50 +0300

    operetörler1

--- script.js
+++ script.js
@@ -111,42 +111,52 @@
 // console.log(typeof total);
 
 
-let val;
+// let val;
 
-//number to string
-val = String(10);
+// //number to string
+// val = String(10);
 
-//bool to string
+// //bool to string
 
-val=String(true);
+// val=String(true);
 
-val= String(new Date());
+// val= String(new Date());
 
-//array to string
+// //array to string
 
-val =String([1,2,3,4]);
+// val =String([1,2,3,4]);
 
-//toString()
+// //toString()
 
-val = (10).toString();
-val = (false).toString();
+// val = (10).toString();
+// val = (false).toString();
 
-//string to number
+// //string to number
 
-val =Number('10');
-val=Number(true);
-val= Number(false);
-val= Number(null);
-val= Number('a');
-val= Number([1,2,3,4]);
+// val =Number('10');
+// val=Number(true);
+// val= Number(false);
+// val= Number(null);
+// val= Number('a');
+// val= Number([1,2,3,4]);
 
-//parsInt
-//parseFloat
+// //parsInt
+// //parseFloat
 
-val=parseInt('10');
-val=parseInt('10.5');
-val=parseFloat('10.5');
+// val=parseInt('10');
+// val=parseInt('10.5');
+// val=parseFloat('10.5');
 
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
+// console.log(val);
+// console.lotypeof val);
+// console.log(val.toFixed(2));
+
+
+
+// ** Müşteri adı 
+// ** Müşteri soyadı
+// ** Müşteri tc  kimlik
+// ** Müşteri sipariş toplamı
+// ** Müşteri cinsiyet
+// ** Müşteri adres bilgisi
+// ** Müşteri hobileri
